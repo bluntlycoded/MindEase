54688df2d7d33936783b07944928768a9386d82c
Rahul Nayak <114763168+bluntlycoded@users.noreply.github.com>
2025-03-19 16:59:58 +0530

    Update adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const { addContent, updateContent, deleteContent, moderateContent } = require('../controllers/adminController');
 const { protect } = require('../middleware/authMiddleware');
 const { adminOnly, moderatorOnly } = require('../middleware/roleMiddleware');
+
 router.post('/content', protect, adminOnly, addContent);
 router.put('/content/:id', protect, adminOnly, updateContent);
 router.delete('/content/:id', protect, adminOnly, deleteContent);
 router.post('/moderate', protect, moderatorOnly, moderateContent);
+
 module.exports = router;
