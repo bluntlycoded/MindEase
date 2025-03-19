7873f6577d35cd690c8da04d1acfeaec56973e80
Rahul Nayak <114763168+bluntlycoded@users.noreply.github.com>
2025-03-19 16:04:15 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,5 @@
 const admin = require('firebase-admin');
-const path = require('path');
-const serviceAccountPath = path.resolve(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
-const serviceAccount = require(serviceAccountPath);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.FIREBASE_DB_URL
